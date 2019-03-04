4d0fab344e4e930a2553d081fc0ef169c5fae841
Luis Jiménez <luisjimenez6245@hotmail.com>
2019-03-04 07:11:16 -0600

    agrega validacions paretesis

--- Practicas/Practica_1/ConvPosfija.c
+++ Practicas/Practica_1/ConvPosfija.c
@@ -18,15 +18,39 @@
 /*
 	comprobarParentesis()
 */
+
+int esSigno(char caracter)
+{
+	int res = 0;
+	if (caracter == '+' || caracter == '-')
+	{
+		res = 1;
+	}
+	else
+	{
+		if (caracter == '*' || caracter == '/')
+		{
+			res = 2;
+		}
+		else
+		{
+			if (caracter == '^')
+				res = 3;
+		}
+	}
+
+	return res;
+}
+
 boolean comprobarParentesis(char expInf[], int tam_cad)
 {
 	stack parentesis;
 	element e;
 	Initialize(&parentesis);
+	boolean res = TRUE;
 	int i = 0;
 	for (i = 0; tam_cad > i; i++)
 	{
-		printf("\n%c",expInf[i]);
 		if (expInf[i] == '(')
 		{
 			e.c = expInf[i];
@@ -34,19 +58,95 @@ boolean comprobarParentesis(char expInf[], int tam_cad)
 		}
 		else if (expInf[i] == ')')
 		{
-			printf("Hola");
 			if (Empty(&parentesis) == FALSE)
 			{
 				Pop(&parentesis);
 			}
 			else
 			{
-				printf("ERROR\n");
-				exit(1);
+				res = FALSE;
+				break;
 			}
 		}
 	}
-	return Empty(&parentesis);
+	if (res != FALSE)
+	{
+		res = Empty(&parentesis);
+	}
+	Destroy(&parentesis);
+	return res;
+}
+
+boolean comprobarSignos(char expInf[], int tam_cad)
+{
+	boolean res = TRUE;
+	stack parentesis, caracteres, signos;
+	Initialize(&parentesis);
+	Initialize(&caracteres);
+	Initialize(&signos);
+	int nivel = 0;
+
+	Destroy(&parentesis);
+	Destroy(&caracteres);
+	Destroy(&signos);
+	return res;
+}
+
+char *cambioPostFijo(char expInf[], int tam_cad)
+{
+	char *res;
+	stack pila;
+	Initialize(&pila);
+	element e;
+	int i = 0;
+	int nivel = 0;
+	for (i = 0; i <= tam_cad; ++i)
+	{
+		e.c = expInf[i];
+		int aux = esSigno(expInf[i]);
+		if (aux > 0)
+		{
+			if (nivel < aux)
+			{
+				Push(&pila, e);
+			}
+			else
+			{
+				res = res + Pop(&pila).c;
+			}
+		}
+		else
+		{
+			if (aux == '(')
+			{
+				nivel = 0;
+				Push(&pila, e);
+			}
+			else
+			{
+				if (aux == ')')
+				{
+					int j;
+					for (j = 0; j < Size(&pila); ++j)
+					{
+						if (Top(&pila).c != '(' && !Empty(&pila))
+							res = res + Pop(&pila).c;
+						else
+							break;
+					}
+				}
+				else
+				{
+					res = res + expInf[i];
+				}
+			}
+		}
+	}
+	if (!Empty(&pila))
+		for (i = 0; i < Size(&pila); ++i)
+			res += Pop(&pila).c;
+	Destroy(&pila);
+	return res;
 }
 
 int main()
@@ -60,10 +160,18 @@ int main()
 		scanf("%[^\n]%*c", expInf);
 		printf("\nComprobando...\n");
 		int tam = strlen(expInf);
-		comprobarParentesis(expInf, tam);
+		if (comprobarParentesis(expInf, tam))
+		{
+			printf("\nParentesís validos\n");
+			printf("%s", cambioPostFijo(expInf, tam));
+		}
+		else
+		{
+			printf("\nParentesis no validos");
+		}
 		printf("\nQuieres volver a introducir una expresion? [S/N]\n");
 		scanf("%c", &respuesta1);
 	}
 	printf("Sale bye!\n");
-	return 0;
+	return EXIT_SUCCESS;
 }
