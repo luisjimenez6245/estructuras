82d6edb9a41b0d78f94505ac4692779ab9457b4d
Luis <luisjimenez6245@hotmail.com>
2019-03-25 22:51:16 -0600

    ya casi

--- Practicas/Practica_2/el perro/procesos.c
+++ Practicas/Practica_2/el perro/procesos.c
@@ -1,10 +1,82 @@
-
+#include <curses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "TADColaDin.h"
 
-elemento getProceso(){
+elemento getProceso();
 
+int main()
+{
+    char res = 's';
+    cola listos, finalizados;
+    elemento aux, anterior;
+    int tiempo = 0;
+    initscr();   
+    Initialize(&listos);
+    Initialize(&finalizados);
+PROCESO:
+    while (res == 's' || res == 'S')
+    {
+        Queue(&listos, getProceso());
+        printw("\nDeseas ingresar más procesos?:\n");
+        scanw("%s", &res);
+        clear();
+        refresh();
+    }
+    printw("\n-------------------------------- INICIO  ---------------------------------\n");
+    while (!Empty(&listos))
+    {
+        aux = Dequeue(&listos);
+        --aux.time;
+        if(aux.time == 0)
+            Queue(&finalizados, aux);
+        else
+            Queue(&listos, aux);
+        tiempo = tiempo + aux.time;
+        move(0,0); 
+        printw("Proceso Anterior:");
+        printw("Proceso Posterior:");
+            if(!Emp)
+        move(0, 40);
+        printw("Proceso actual:")
+        printw("Nombre: %s", aux.processName);
+        printw("ID: %s", aux.id);
+        printw("Actividad: %s", aux.activity);
+        printw("Tiempo: %i", aux.time);
+
+        clear();
+        refresh();
+    }
+    printw("\n--------------------------------   FIN   ---------------------------------\n");
+    printw("\n---------------------------------- RESUMEN -----------------------------------\n");
+    while (!Empty(&finalizados))
+    {
+
+    }
+    printw("\nTiempo total: %i", tiempo);
+    printw("\nQuieres repetir el programa?:\n");
+    scanw("%s", &res);
+    if (res == 's' || res == 'S')
+        goto PROCESO;
+    Destroy(&finalizados);
+    Destroy(&listos);
+     endwin();
+    return EXIT_SUCCESS;
 }
 
-int main(){
-
+elemento getProceso()
+{
+    elemento e;
+    printw("Nuevo Proceso\n");
+    printw("Ingresa nombre del proceso:\n");
+    scanw("%[^\n]%*c",e.processName);
+    printw("Ingresa actividad del proceso:\n");
+    scanw("%[^\n]%*c",e.activity);
+    printw("Ingresa id del proceso:\n");
+    scanw("%s", e.id);
+    printw("Ingresa tiempo del proceso:\n");
+    scanw("%i", &e.time);
+    fflush(stdin);
+    return e;
 }
