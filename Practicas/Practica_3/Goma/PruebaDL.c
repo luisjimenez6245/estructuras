6c86758545e01b739c9e37ea6d83f64e4cad8bc7
Luis <luisjimenez6245@hotmail.com>
2019-04-12 23:58:38 -0500

    adds gomita changes

---
+++ Practicas/Practica_3/Goma/PruebaDL.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "TADListaDL.h"
+
+int main(){
+    lista list;
+    posicion pos;
+    elemento e;
+    int i = 0;
+
+    Initialize(&list);
+    for(i = 0; i <10; ++i){
+        Add(&list, e);
+    }
+    pos = First(&list);
+    Following(&list,pos);
+    VerLigasLista(&list);
+    Destroy(&list);
+}
