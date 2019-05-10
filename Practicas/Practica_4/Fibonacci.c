e0227d2d46dfedb0ed510e4045ac272869e70b82
Aarón Gama <25625929+ElWakamolexD@users.noreply.github.com>
2019-05-10 18:42:50 -0500

    Reporte agregado
    Se corrigieron los programas de fibonacci y tribonacci, además se agregó el reporte

--- Practicas/Practica_4/Fibonacci.c
+++ Practicas/Practica_4/Fibonacci.c
@@ -1,24 +1,29 @@
-/*
-    FIBONACCI
-*/
 #include <stdio.h>
 #include <stdlib.h>
 
-void fibonacci(int n,int m, int limit){
+/*
+	FUNCIÓN: fibonacci (int n,int m, int limit,int a)
+	RECIBE: int n (PRIMER NÚMERO A SUMAR), int m (SEGUNDO NÚMERO A SUMAR),
+		int limit (POSICIÓN LÍMITE), int a (POSICIÓN ACTUAL).
+	DEVUELVE: SE LLAMA A SÍ MISMA SI ES NECESARIO.
+	DESCRIPCIÓN: FUNCIÓN ENCARGADA DE CALCULAR LOS NÚMEROS DE LA SERIE DE FIBONACCI.
+	OBSERVACIONES: CUANDO EL NÚMERO ES MUY GRANDE IMPRIME NEGATIVOS.
+*/
+void fibonacci(int n,int m, int limit, int a){
     if(n==0&&m==1){
         printf("0, 1, ");
     }
-    if(m>=limit){
+    if(a==limit){
         return;
     }
     printf("%i, ",m+n);
-    fibonacci(m,m+n,limit);
+    fibonacci(m,m+n,limit,a+1);
 }
 
 int main(){
     int n;
     printf("Ingresa el numero limite:\n>");
     scanf("%i",&n);
-    fibonacci(0,1,n);
+    fibonacci(0,1,n,1);
     return 0;
 }
--- Practicas/Practica_4/Tribonacci.c
+++ Practicas/Practica_4/Tribonacci.c
@@ -4,21 +4,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void tribonacci(int x, int y, int z, int limit){
+/*
+	FUNCIÓN: tribonacci (int x,int y, int z, int limit)
+	RECIBE: int x (PRIMER NÚMERO A SUMAR), int y (SEGUNDO NÚMERO A SUMAR),
+		int z (TERCER NÚMERO A SUMAR), int limit (POSICIÓN LÍMITE), int n 
+		(POSICIÓN ACTUAL).
+	DEVUELVE: SE LLAMA A SÍ MISMA SI ES NECESARIO.
+	DESCRIPCIÓN: FUNCIÓN ENCARGADA DE CALCULAR LOS NÚMEROS DE LA SERIE DE TRIBONACCI.
+	OBSERVACIONES: CUANDO EL NÚMERO ES MUY GRANDE IMPRIME NEGATIVOS.
+*/
+void tribonacci(int x, int y, int z, int limit, int n){
     if(x==0&&y==0&&z==1){
         printf("0, 0, 1, ");
     }
-    if(z>=limit){
+    if(n>=limit){
         return;
     }
     printf("%i, ",x+y+z);
-    tribonacci(y,z,x+y+z,limit);
+    tribonacci(y,z,x+y+z,limit,n+1);
 }
 
 int main(){
     int n;
-    printf("Ingresa el nÃºmero limite:\n>");
+    printf("Ingresa el numero limite:\n>");
     scanf("%i",&n);
-    tribonacci(0,0,1,n);
+    tribonacci(0,0,1,n,1);
     return 0;
 }
